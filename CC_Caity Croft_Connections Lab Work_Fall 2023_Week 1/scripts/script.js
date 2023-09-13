7a620254bb11c38b09741e83804436f27647218d
Caity Croft <106709245+caitycroft@users.noreply.github.com>
2023-09-13 10:27:21 -0400

    Added JS to Button Parameters
    Thank you Dora! :)

--- CC_Caity Croft_Connections Lab Work_Fall 2023_Week 1/scripts/script.js
+++ CC_Caity Croft_Connections Lab Work_Fall 2023_Week 1/scripts/script.js
@@ -1,10 +1,83 @@
+let isTechnoClicked = false;
+let isRockClicked = false;
+let isOperaClicked = false;
+
+
+/*var booleanValue = true;
+
+function someFunction(){
+    if(booleanValue){
+        return "something";
+    }
+}*/
+
+
+document.getElementById("techno_button").addEventListener("click", function () {
+    console.log("techno dance")
+    document.getElementById("techno_image").style.display = "block"
+    isTechnoClicked = true;
+    if (isRockClicked) {
+        isRockClicked = false
+        document.getElementById("rock_image").style.display = "none"
+
+    }
+
+    if (isOperaClicked) {
+        isOperaClicked = false
+        document.getElementById("opera_image").style.display = "none"
+
+    }
+}
+
+);
+
+document.getElementById("rock_button").addEventListener("click", function () {
+    console.log("I wanna rock n roll all night")
+    document.getElementById("rock_image").style.display = "block"
+    isRockClicked = true;
+    if (isTechnoClicked) {
+        isTechnoClicked = false
+        document.getElementById("techno_image").style.display = "none"
+
+    }
+
+    if (isOperaClicked) {
+        isOperaClicked = false
+        document.getElementById("opera_image").style.display = "none"
+
+    }
+}
+
+);
 
 
 document.getElementById("opera_button").addEventListener("click", function () {
     console.log("happy dance")
     document.getElementById("opera_image").style.display = "block"
+    isOperaClicked = true;
+    if (isTechnoClicked) {
+        isTechnoClicked = false
+        document.getElementById("techno_image").style.display = "none"
+
+    }
+    if (isRockClicked) {
+        isRockClicked = false
+        document.getElementById("rock_image").style.display = "none"
+
+    }
 }
 
 
 );
 
+
+
+
+/*Stack Overflow Recommeneded Template
+    $("#formButton").click(function () {
+    $("#form1").toggle();
+    });
+
+    $("#formButton2").click(function () {
+    $("#form2").toggle();
+}   );*/
